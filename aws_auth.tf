8d6177a8580f47aea9ca5b051cf37f2d8d081e62
opzsatish <srundla@opz0.io>
2023-11-30 16:06:33 +0530

    update fix issue

--- aws_auth.tf
+++ aws_auth.tf
@@ -41,7 +41,7 @@ data "aws_eks_cluster_auth" "eks" {
 provider "kubernetes" {
   token                  = join("", data.aws_eks_cluster_auth.eks[*].token)
   host                   = join("", data.aws_eks_cluster.eks[*].endpoint)
-  cluster_ca_certificate = base64decode(join("", data.aws_eks_cluster.eks[*].certificate_authority[*].data))
+  cluster_ca_certificate = base64decode(join("", data.aws_eks_cluster.eks[*].certificate_authority[0].data))
 }
 
 resource "kubernetes_config_map" "aws_auth_ignore_changes" {
